370000dd3f53ad5e88dc196389971722d4ddff7b
Nikolai Hartmann <nikoladze@posteo.de>
2021-12-10 09:25:41 +0100

    add day10 solution

---
+++ day10/solver.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+
+from functools import wraps
+from datetime import datetime
+
+
+times = []
+
+
+def measure_time(func):
+    @wraps(func)
+    def _func(*args, **kwargs):
+        start = datetime.now()
+        result = func(*args, **kwargs)
+        end = datetime.now()
+        times.append((func.__name__, (end - start).total_seconds()))
+        return result
+
+    return _func
+
+
+@measure_time
+def parse(raw_data):
+    return [line for line in raw_data.split("\n")]
+
+
+MATCHING_PARENS = {"{": "}", "[": "]", "(": ")", "<": ">"}
+
+ERR = None
+EXP = None
+
+def check_line(line, pos=0, expected=None):
+    global ERR
+    global EXP
+    if len(line) == 0:
+        EXP = expected
+        raise StopIteration("Done")
+    first = line[0]
+    if first in "([{<":
+        opening = first
+        closing, rest, pos = check_line(line[1:], pos + 1, expected=MATCHING_PARENS[opening])
+        if closing != MATCHING_PARENS[opening]:
+            ERR = closing, pos
+            raise SyntaxError(f"Expected {MATCHING_PARENS[opening]}, but found {closing} instead. Rest: {rest}")
+        return check_line(rest, pos + 1, expected=expected)
+    else:
+        return first, line[1:], pos
+
+
+ERR_SCORES = {
+    ")": 3,
+    "]": 57,
+    "}": 1197,
+    ">": 25137,
+}
+
+
+# PART 1
+@measure_time
+def solve1(data):
+    score = 0
+    for line in data:
+        print(line)
+        try:
+            check_line(line)
+        except StopIteration:
+            print(EXP)
+        except SyntaxError as e:
+            print(e)
+            score += ERR_SCORES[ERR[0]]
+    return score
+
+
+def complete(line):
+    closing_sequence = []
+    while True:
+        try:
+            check_line(line + "".join(closing_sequence))
+        except StopIteration:
+            if EXP is not None:
+                closing_sequence.append(EXP)
+            else:
+                return "".join(closing_sequence)
+        except SyntaxError as e:
+            return
+
+COMPLETION_ERR_SCORES = {
+    ")": 1,
+    "]": 2,
+    "}": 3,
+    ">": 4,
+}
+
+
+# PART 2
+@measure_time
+def solve2(data):
+    scores = []
+    for line in data:
+        closing_sequence = complete(line)
+        score = 0
+        if closing_sequence is not None:
+            for c in closing_sequence:
+                score = score * 5 + COMPLETION_ERR_SCORES[c]
+            scores.append(score)
+    return sorted(scores)[len(scores) // 2]
+
+if __name__ == "__main__":
+    import sys
+
+    data = parse(open("input.txt").read().strip())
+    print("Part 1: {}".format(solve1(data)))
+    print("Part 2: {}".format(solve2(data)))
+
+    print("\nTime taken:")
+    for func, time in times:
+        print(f"{func:8}{time}s")
+    print("----------------")
+    print("total   {}s".format(sum(t for _, t in times)))
