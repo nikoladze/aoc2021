636a26fbb81ccf7530c21de1b8b3a441f8c61694
Nikolai Hartmann <nikoladze@posteo.de>
2021-12-12 11:35:16 +0100

    cleanup

--- day12/solver.py
+++ day12/solver.py
@@ -25,78 +25,64 @@ def parse(raw_data):
     return [line.split("-") for line in raw_data.split("\n")]
 
 
-# PART 1
-@measure_time
-def solve1(data):
+def possible_paths_from(current_path, edges_from, stopping_condition):
+    src = current_path[-1]
+    if src == "end":
+        return [current_path]
+    paths = []
+    for dst in edges_from[src]:
+        if stopping_condition(dst, current_path):
+            continue
+        for path in possible_paths_from(current_path + [dst], edges_from, stopping_condition):
+            if len(path) == 0:
+                continue
+            if path[-1] == "end":
+                paths.append(path)
+    return paths
+
+
+def get_edges_from(data):
     edges_from = defaultdict(list)
     for src, dst in data:
         edges_from[src].append(dst)
         edges_from[dst].append(src)
+    return edges_from
 
-    #visited_big = set()
-    #possible_paths = []
 
-    def possible_paths_from(current_path):
-        #print(f"possible_paths_from({current_path})")
-        src = current_path[-1]
-        if src == "end":
-            return [current_path]
-        paths = []
-        #print(f"edges from {src}: {edges_from[src]}")
-        for dst in edges_from[src]:
-            #print(f"My path so far: {current_path}, Looking for paths through {dst}")
-            if dst.islower() and dst in current_path:
-                #print(f"{dst} is a small cave already visited!")
-                continue
-            for path in possible_paths_from(current_path + [dst]):
-                if len(path) == 0:
-                    continue
-                if path[-1] == "end":
-                    paths.append(path)
-        return paths
-
-    possible_paths = possible_paths_from(["start"])
-
-    return len(possible_paths)
+# PART 1
+@measure_time
+def solve1(data):
+    return len(
+        possible_paths_from(
+            ["start"],
+            get_edges_from(data),
+            # visit small caves at most once
+            lambda dst, current_path: dst.islower() and dst in current_path,
+        )
+    )
 
 
 # PART 2
 @measure_time
 def solve2(data):
-    edges_from = defaultdict(list)
-    for src, dst in data:
-        edges_from[src].append(dst)
-        edges_from[dst].append(src)
-
-    def possible_paths_from(current_path):
-        #print(f"possible_paths_from({current_path})")
-        src = current_path[-1]
-        if src == "end":
-            return [current_path]
-        paths = []
-        #print(f"edges from {src}: {edges_from[src]}")
-        for dst in edges_from[src]:
-            #print(f"My path so far: {current_path}, Looking for paths through {dst}")
-            if dst == "start":
-                continue
-            if (
+    return len(
+        possible_paths_from(
+            ["start"],
+            get_edges_from(data),
+            lambda dst, current_path: (
+                # don't go back to start
+                dst == "start"
+                # visit at most one small cave twice
+                or (
                     dst.islower() and dst in current_path
-                    and any(current_path.count(other) >= 2 for other in set(current_path) if other.islower())
-            ):
-                # print(f"{dst} is a small cave already visited!")
-                continue
-
-            for path in possible_paths_from(current_path + [dst]):
-                if len(path) == 0:
-                    continue
-                if path[-1] == "end":
-                    paths.append(path)
-        return paths
-
-    possible_paths = possible_paths_from(["start"])
-
-    #breakpoint()
-    return len(possible_paths)
+                    and any(
+                        current_path.count(other) >= 2
+                        for other in set(current_path) if other.islower()
+                    )
+                )
+            ),
+        )
+    )
 
 
 if __name__ == "__main__":
