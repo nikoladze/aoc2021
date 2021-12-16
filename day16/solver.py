f7c0afe21c8704f1ac9d90df8a8754cfd5be8e35
Nikolai Hartmann <nikoladze@posteo.de>
2021-12-16 14:03:54 +0100

    cleanup

--- day16/solver.py
+++ day16/solver.py
@@ -48,80 +48,61 @@ class Decoder:
         self.pos = 0
         self.data = data
         self.version_sum = 0
-        print("")
-        print(self.data)
 
     def read(self, n, as_bits=False):
-        #print(f"Reading {n} bits at position {self.pos}")
-        #print(f"{self.data[self.pos:]}")0
         pos = self.pos
         self.pos += n
-        print(self.data)
-        print(" " * self.pos + "^")
         out = self.data[pos : pos + n]
         if as_bits:
             return out
         else:
             return int(out, 2)
 
+    def parse_literal(self):
+        value_bits = []
+        last_group = False
+        while not last_group:
+            last_group = self.read(1) == 0
+            value_bits.append(self.read(4, as_bits=True))
+        return int("".join(value_bits), 2)
+
+    def parse_operator(self, type_id):
+        length_type_id = self.read(1)
+        results = []
+        if length_type_id == 0:
+            total_length = self.read(15)
+            stop = self.pos + total_length
+            while self.pos != stop:
+                results.append(self.parse())
+        else:
+            number_of_subpackets = self.read(11)
+            for i in range(number_of_subpackets):
+                results.append(self.parse())
+        if type_id == 0:
+            return sum(results)
+        elif type_id == 1:
+            return reduce(mul, results)
+        elif type_id == 2:
+            return min(results)
+        elif type_id == 3:
+            return max(results)
+        elif type_id == 5:
+            return 1 if results[0] > results[1] else 0
+        elif type_id == 6:
+            return 1 if results[0] < results[1] else 0
+        elif type_id == 7:
+            return 1 if results[0] == results[1] else 0
+        raise Exception(f"Unknown type_id {type_id}")
 
     def parse(self):
         while self.pos < len(self.data):
             packet_version = self.read(3)
             self.version_sum += packet_version
             type_id = self.read(3)
-            print(f"packet_version: {packet_version}")
-            print(f"type_id: {type_id}")
             if type_id == 4:
-                # read literal values until first bit is 0
-                value_bits = []
-                while True:
-                    last_group = self.read(1) == 0
-                    print("decode a literal packet")
-                    value_bits.append(self.read(4, as_bits=True))
-                    if last_group:
-                        print("done!")
-                        return int("".join(value_bits), 2)
-                # # discard trailing 0s
-                # if self.pos % 4 != 0:
-                #     print("discard trailing 0s")
-                #     _ = self.read(4 - self.pos % 4)
+                return self.parse_literal()
             else:
-                # this is an operator packet
-                print("decode an operator packet")
-                length_type_id = self.read(1)
-                results = []
-                if length_type_id == 0:
-                    total_length = self.read(15)
-                    print(f"trying to read {total_length} bits of sub-packets")
-                    stop = self.pos + total_length
-                    while self.pos != stop:
-                        results.append(self.parse())
-                else:
-                    number_of_subpackets = self.read(11)
-                    print(f"trying to read {number_of_subpackets} sub-packets")
-                    for i in range(number_of_subpackets):
-                        results.append(self.parse())
-
-                if type_id == 0:
-                    return sum(results)
-                elif type_id == 1:
-                    return reduce(mul, results)
-                elif type_id == 2:
-                    return min(results)
-                elif type_id == 3:
-                    return max(results)
-                elif type_id == 5:
-                    return 1 if results[0] > results[1] else 0
-                elif type_id == 6:
-                    return 1 if results[0] < results[1] else 0
-                elif type_id == 7:
-                    return 1 if results[0] == results[1] else 0
-                raise Exception("wat?")
-
-
-
-
+                return self.parse_operator(type_id)
 
 
 # PART 1
