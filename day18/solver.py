19619a73e7b76741e17a0894fa265aac666761a1
Nikolai Hartmann <nikoladze@posteo.de>
2021-12-18 10:02:02 +0100

    add solution for day18

---
+++ day18/solver.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+
+from functools import wraps
+from datetime import datetime
+import math
+
+
+times = []
+
+
+def measure_time(func):
+    @wraps(func)
+    def _func(*args, **kwargs):
+        start = datetime.now()
+        result = func(*args, **kwargs)
+        end = datetime.now()
+        times.append((func.__name__, (end - start).total_seconds()))
+        return result
+
+    return _func
+
+
+def tokenize(line):
+    return [t for t in line if t != ","]
+
+
+@measure_time
+def parse(raw_data):
+    return [tokenize(line) for line in raw_data.split("\n")]
+
+
+def find_exploding_pair(tokens):
+    level = 0
+    for i, t in enumerate(tokens):
+        if t == "[":
+            level += 1
+        if t == "]":
+            level -= 1
+        if level > 4 and tokens[i + 1].isnumeric() and tokens[i + 2].isnumeric():
+            return i
+
+
+def find_large_number(tokens):
+    for i, t in enumerate(tokens):
+        if t.isnumeric() and int(t) >= 10:
+            return i
+
+
+def explode_pair_at(tokens, i):
+    tokens.pop(i) # [
+    left = tokens.pop(i)
+    right = tokens.pop(i)
+    tokens.pop(i) # ]
+    for j in range(i - 1, -1, -1):
+        if tokens[j].isnumeric():
+            tokens[j] = str(int(left) + int(tokens[j]))
+            break
+    for j in range(i, len(tokens)):
+        if tokens[j].isnumeric():
+            tokens[j] = str(int(right) + int(tokens[j]))
+            break
+    tokens.insert(i, "0")
+
+
+def split_number_at(tokens, i):
+    n = int(tokens.pop(i))
+    tokens.insert(i, "]")
+    tokens.insert(i, str(math.ceil(n / 2)))
+    tokens.insert(i, str(math.floor(n / 2)))
+    tokens.insert(i, "[")
+
+
+def reduce_snailnum(tokens):
+    while True:
+        if (i := find_exploding_pair(tokens)) is not None:
+            explode_pair_at(tokens, i)
+            continue
+        if (i := find_large_number(tokens)) is not None:
+            split_number_at(tokens, i)
+            continue
+        break
+
+
+def add_snailnums(tokens1, tokens2):
+    sum_tokens = ["["] + tokens1 + tokens2 + ["]"]
+    reduce_snailnum(sum_tokens)
+    return sum_tokens
+
+
+def sum_snailnums(token_list):
+    total = token_list[0]
+    for other in token_list[1:]:
+        total = add_snailnums(total, other)
+    return total
+
+
+def to_string(tokens):
+    return ",".join(tokens).replace("[,", "[").replace(",]", "]")
+
+
+def reduce_magnitude(tokens):
+    tokens = tokens[:]
+    while True:
+        for i, t in enumerate(tokens):
+            if t == "[" and tokens[i + 1].isnumeric() and tokens[i + 2].isnumeric():
+                tokens.pop(i)
+                magnitude = 3 * int(tokens.pop(i))
+                magnitude += 2 * int(tokens.pop(i))
+                tokens.pop(i)
+                tokens.insert(i, str(magnitude))
+                break
+        else:
+            break
+    return tokens
+
+
+# PART 1
+@measure_time
+def solve1(data):
+    return int(reduce_magnitude(sum_snailnums(data))[0])
+
+
+# PART 2
+@measure_time
+def solve2(data):
+    magnitudes = []
+    for n1 in data:
+        for n2 in data:
+            magnitudes.append(int(reduce_magnitude(add_snailnums(n1, n2))[0]))
+    return max(magnitudes)
+
+
+
+if __name__ == "__main__":
+    import sys
+
+    data = parse(open("input.txt").read().strip())
+    print("Part 1: {}".format(solve1(data)))
+    print("Part 2: {}".format(solve2(data)))
+
+    print("\nTime taken:")
+    for func, time in times:
+        print(f"{func:8}{time}s")
+    print("----------------")
+    print("total   {}s".format(sum(t for _, t in times)))
---
+++ day18/test_solver.py
@@ -0,0 +1,79 @@
+import pytest
+from solver import (
+    tokenize,
+    parse,
+    solve1,
+    solve2,
+    find_exploding_pair,
+    explode_pair_at,
+    reduce_snailnum,
+    sum_snailnums,
+    to_string,
+)
+
+TESTDATA = """"""
+
+@pytest.fixture
+def parsed_data():
+    return parse(TESTDATA)
+
+
+def test_parse():
+    data = parse(TESTDATA)
+    # asserts go here
+
+def test_explode():
+
+    def explode(s):
+        tokens = tokenize(s)
+        pos = find_exploding_pair(tokens)
+        explode_pair_at(tokens, pos)
+        return to_string(tokens)
+
+    assert explode("[[[[[9,8],1],2],3],4]") == "[[[[0,9],2],3],4]"
+    assert explode("[7,[6,[5,[4,[3,2]]]]]") == "[7,[6,[5,[7,0]]]]"
+    assert explode("[[6,[5,[4,[3,2]]]],1]") == "[[6,[5,[7,0]]],3]"
+    assert explode("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]") == "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"
+    assert explode("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]") == "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"
+
+
+def test_reduce():
+
+    def reduce(s):
+        tokens = tokenize(s)
+        reduce_snailnum(tokens)
+        return to_string(tokens)
+
+    assert reduce("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]") == "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
+
+
+def test_sum():
+
+    def sum(s):
+        return to_string(sum_snailnums(parse(s)))
+
+    assert sum("""[1,1]
+[2,2]
+[3,3]
+[4,4]""") == "[[[[1,1],[2,2]],[3,3]],[4,4]]"
+    assert sum("""[1,1]
+[2,2]
+[3,3]
+[4,4]
+[5,5]""") == "[[[[3,0],[5,3]],[4,4]],[5,5]]"
+    assert sum("""[1,1]
+[2,2]
+[3,3]
+[4,4]
+[5,5]
+[6,6]""") == "[[[[5,0],[7,4]],[5,5]],[6,6]]"
+    assert sum("""[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
+[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
+[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
+[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
+[7,[5,[[3,8],[1,4]]]]
+[[2,[2,2]],[8,[8,1]]]
+[2,9]
+[1,[[[9,3],9],[[9,0],[0,7]]]]
+[[[5,[7,4]],7],1]
+[[[[4,2],2],6],[8,7]]""") == "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"
