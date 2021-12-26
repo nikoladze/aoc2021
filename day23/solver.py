a27eca14e946e19c6df1bb93ae88125ed1e02e18
Nikolai Hartmann <nikoladze@posteo.de>
2021-12-26 11:46:11 +0100

    possible solution

--- day23/solver.py
+++ day23/solver.py
@@ -27,66 +27,68 @@ def parse(raw_data):
     return out
 
 
-#global_history = set()
-
 class AmphiGame:
     def __init__(self, data):
         self.map = data
-        #self.possible_pos = sum([[(i, j) for j, v in enumerate(line) if v in ".ABCD"] for i, line in enumerate(self.map)], [])
         self.destinations = dict(zip("ABCD", [3, 5, 7, 9]))
-        # self.destinations = dict(zip("ABCD", range(4)))
         self.energies = dict(zip("ABCD", map(lambda x: 10 ** x, range(4))))
-        # self.hallway = ["."] * 11
-        # self.entrances = [2, 4, 6, 8]
-        # self.map = [self.hallway] + self.rooms
         self._highlight = None
+        self._occupied = None
+        self._unoccupied = None
 
     @property
     def occupied(self):
+        if self._occupied is not None:
+            return self._occupied
+        self._occupied = []
         for y, line in enumerate(self.map):
             for x, v in enumerate(line):
                 if v in "ABCD":
-                    yield (x, y)
+                    self._occupied.append((x, y))
+        return self._occupied
 
-    def possible_step(self, src, dst):
-        x1, y1 = src
-        x2, y2 = dst
-
-        # spot free?
-        if self.map[y2][x2] != ".":
-            return False
-
-        # adjacent?
-        if not (abs(x2 - x1) == 1 or abs(y2 - y1) == 1):
-            return False
-
-        # if in hallway i can only go to final destination
-        # (and the other one also has to be there)
-        # if y2 not in (2, 3):
-        #     val = self.map[y1][x1]
-        #     if self.destinations[val] != x2:
-        #         return False
-        #     if self.map[[y for y in (2, 3) if not y == y2][0]][x2] != val:
-        #         return False
-
-        return True
+    @property
+    def unoccupied(self):
+        if self._unoccupied is not None:
+            return self._unoccupied
+        self._unoccupied = []
+        for y, line in enumerate(self.map):
+            for x, v in enumerate(line):
+                if v == ".":
+                    self._unoccupied.append((x, y))
+        return self._unoccupied
 
     def possible_move(self, src, dst):
         x1, y1 = src
         x2, y2 = dst
 
-        # moving to a spot in front of a room is not allowed
-        if y2 ==1 and x2 in (3, 5, 7, 9):
+        # spot free/visitable?
+        if self.map[y2][x2] != ".":
+            assert False ### TODO
             return False
 
+        # moving to a spot in front of a room is not allowed
+        if y2 == 1 and x2 in (3, 5, 7, 9):
+            return False
+
+        # can't go from hallway to hallway
+        if y1 == 1 and y2 == 1:
+            return False
+
+        # we have to go to the hallway if we are in the room
+        if y1 in (2, 3)  and y2 != 1:
+            return False
+
+        # if that room is done, we stay there
         if y1 in (2, 3):
             val = self.map[y1][x1]
-            # if we are in final destination and the other field is also done, we stay there
-            if self.destinations[val] == x2 and self.map[[y for y in (2, 3) if not y == y2][0]][x2] == val:
-                return False
-            # otherwise moving from side room to hallway is fine
-            if y2 == 1:
-                return True
+            other_y = 2 if y1 == 3 else 3
+            other_val = self.map[other_y][x1]
+            if self.destinations[val] == x1:
+                if val == other_val:
+                    return False
+                if y1 == 3:
+                    return False
 
         # if in hallway i can only go to final destination
         # (and the other one also has to be there or empty)
@@ -94,29 +96,33 @@ class AmphiGame:
             val = self.map[y1][x1]
             if self.destinations[val] != x2:
                 return False
-            if self.map[[y for y in (2, 3) if not y == y2][0]][x2] not in (val, "."):
+            if y2 == 2 and self.map[3][x2] != val:
                 return False
+
+        # if no contiguous path of free fields between src and dst we can't do this
+        if self.possible_path_distance(src, dst) is None:
+            return False
 
         return True
 
-    def possible_paths_from(self, current_path):
-        """
-        TODO: just check start and endpoints, then have energy function
-        """
+    def possible_path_distance(self, src, dst, current_path=None):
+        "this routine might be problematic ..."
+        if current_path is None:
+            current_path = [src]
         src = current_path[-1]
-        paths = []
+        distance = None
         for step in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
-            dst = (src[0] + step[0], src[1] + step[1])
-            if not self.possible_step(src, dst):
+            pos = (src[0] + step[0], src[1] + step[1])
+            if pos == dst:
+                return len(current_path)
+            if self.map[pos[1]][pos[0]] != ".":
                 continue
-            if dst in current_path:
+            if pos in current_path:
                 # don't go back
                 continue
-            for path in self.possible_paths_from(current_path + [dst]):
-                paths.append(path)
-        if len(paths) == 0:
-            return [current_path]
-        return paths
+            if (distance := self.possible_path_distance(pos, dst, current_path + [pos])) is not None:
+                break
+        return distance
 
     @property
     def is_done(self):
@@ -138,84 +144,39 @@ class AmphiGame:
     def map_key(self):
         return tuple(tuple(line) for line in self.map)
 
-    def find_min_energy(self, energy_so_far=0, min_energy=None, history=None):
+    def find_min_energy(self, energy_so_far=0, min_energy=None, history=None, indent=""):
         if history is None:
             history = set([self.map_key])
-        #energies = []
         for src in self.occupied:
             c = self.map[src[1]][src[0]]
-            #self.print_highlight(src)
-            for path in self.possible_paths_from([src]):
-                energy = energy_so_far
-                for pos in path[1:]:
-                    energy += self.energies[c]
-                    if min_energy is not None and energy >= min_energy:
-                        continue
-                    if not self.possible_move(src, pos):
-                        continue
-                    game = AmphiGame([line[:] for line in self.map])
-                    game.map[src[1]][src[0]] = "."
-                    game.map[pos[1]][pos[0]] = c
-                    if game.map_key in history:
-                        continue
-                    # if game.map_key in global_history:
-                    #     breakpoint()
-                    # if game.map_key in global_history:
-                    #     continue
-                    #global_history.add(game.map_key)
-                    #print(len(global_history))
-                    #breakpoint()
-                    if game.is_done:
-                        #breakpoint()
-                        print("Done!")
-                        print(energy)
-                        # energies.append(energy)
-                        # continue
-                        min_energy_next = energy
-                    else:
-                        min_energy_next = game.find_min_energy(
-                            energy_so_far=energy,
-                            min_energy=min_energy,
-                            history=history | set([game.map_key])
-                        )
-                    if min_energy_next is None:
-                        continue
-                    if min_energy is None or min_energy_next < min_energy:
-                        min_energy = min_energy_next
-                    # if min_energy_next is None:
-                    #     continue
-                    # energies.append(min_energy_next)
-        # if len(energies) == 0:
-        #     return None
-        # return min(energies)
+            for pos in self.unoccupied:
+                if not self.possible_move(src, pos):
+                    continue
+                distance = self.possible_path_distance(src, pos)
+                energy = energy_so_far + self.energies[c] * distance
+                if min_energy is not None and energy >= min_energy:
+                    continue
+                game = AmphiGame([line[:] for line in self.map])
+                game.map[src[1]][src[0]] = "."
+                game.map[pos[1]][pos[0]] = c
+                if game.map_key in history:
+                    continue
+                if game.is_done:
+                    print("Done!")
+                    print(energy)
+                    min_energy_next = energy
+                else:
+                    min_energy_next = game.find_min_energy(
+                        energy_so_far=energy,
+                        min_energy=min_energy,
+                        history=history | set([game.map_key]),
+                        indent=indent + " "
+                    )
+                if min_energy_next is None:
+                    continue
+                if min_energy is None or min_energy_next < min_energy:
+                    min_energy = min_energy_next
         return min_energy
-
-
-    # def find_min_energy(self):
-    #     for src in self.occupied:
-    #         for step in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
-    #             dst = (src[0] + step[0], src[1] + step[1])
-    #         if not self.possible_step(src, dst):
-    #             continue
-    #         if dst in current_path:
-    #             # don't go back
-    #             continue
-    #         for path in self.possible_paths_from(current_path + [dst]):
-    #             paths.append(path)
-    #     src = current_path[-1]
-    #     paths = []
-    #     for step in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
-    #         dst = (src[0] + step[0], src[1] + step[1])
-    #         if not self.possible_step(src, dst):
-    #             continue
-    #         if dst in current_path:
-    #             # don't go back
-    #             continue
-    #         for path in self.possible_paths_from(current_path + [dst]):
-    #             paths.append(path)
-    #     if len(paths) == 0:
-    #         return [current_path]
-    #     return paths
 
     def __repr__(self):
         return "\n".join(
@@ -226,9 +187,10 @@ class AmphiGame:
             for y, line in enumerate(self.map)
         )
 
-    def print_highlight(self, pos):
+    def print_highlight(self, pos, indent=""):
         self._highlight = pos
-        print(self)
+        for line in self.__repr__().split("\n"):
+            print(indent + line)
         self._highlight = None
 
 
@@ -247,7 +209,6 @@ def solve2(data):
 
 if __name__ == "__main__":
     import sys
-    sys.setrecursionlimit(10000)
 
     data = parse(open("input.txt").read().strip())
     print("Part 1: {}".format(solve1(data)))
@@ -260,8 +221,8 @@ if __name__ == "__main__":
     print("total   {}s".format(sum(t for _, t in times)))
 
     # interactive testing
-    #game = AmphiGame(data)
+    # game = AmphiGame(data)
 
     # interactive testing
-    from test_solver import TESTDATA
-    game = AmphiGame(parse(TESTDATA.strip()))
+    # from test_solver import TESTDATA
+    # game = AmphiGame(parse(TESTDATA.strip()))
